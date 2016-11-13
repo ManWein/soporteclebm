55cd43224a0e9a583e27c68f733b67f5c12ab67a
Manuel W <manuel@softwarecriollo.com>
2016-11-12 23:49:35 -0400

    Ultimo commit antes de entregar

--- app/models/applicant_info.rb
+++ app/models/applicant_info.rb
@@ -1,8 +1,13 @@
 class ApplicantInfo < ActiveRecord::Base
   belongs_to :applicant
+  belongs_to :office
 
   accepts_nested_attributes_for :applicant, allow_destroy: true
-
   validates :name, :username, :cedula, :tlf, :cargo, :office_id, presence: true
 
+  def oficina_nombre
+    self.office.blank? ? "N/A" : self.office.set_nombre
+  end
+
 end
+
--- app/models/office.rb
+++ app/models/office.rb
@@ -1,7 +1,11 @@
 class Office < ActiveRecord::Base
+  has_one :applicant_info
 
   def self.permited_params
     [:nombre, :estatus]
   end
 
+  def set_nombre
+    self.nombre.blank? ? "N/A" : self.nombre
+  end
 end
